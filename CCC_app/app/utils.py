ff9f9afaefb566a77a88ba5a894f98afd027fbec
BDMaurer <125302800+BDMaurer@users.noreply.github.com>
2024-09-25 14:13:29 -0400

    Update utils.py

--- CCC_app/app/utils.py
+++ CCC_app/app/utils.py
@@ -1,1 +1,22 @@
+from functools import wraps
+from flask import abort
+from flask_login import current_user
 
+def permission_required(permission_level):
+    def decorator(f):
+        @wraps(f)
+        def decorated_function(*args, **kwargs):
+            if not current_user.is_authenticated or current_user.PermissionTier < permission_level:
+                abort(403)
+            return f(*args, **kwargs)
+        return decorated_function
+    return decorator
+
+def admin_required(f):
+    return permission_required(3)(f)
+
+def super_admin_required(f):
+    return permission_required(4)(f)
+
+def employee_required(f):
+    return permission_required(2)(f)
