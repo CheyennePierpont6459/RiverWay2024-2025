a7c7f628ec1035d5d23622ff59035907eda7fdac
Bailey P Forbes <62630945+BPForbes@users.noreply.github.com>
2024-10-24 20:47:48 -0400

    Delete CCC_app directory

--- CCC_app/CCC_app/config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'secret_key'
-    MYSQL_HOST = 'localhost'
-    MYSQL_USER = 'root'
-    MYSQL_PASSWORD = 'Preston-2020'
-    MYSQL_DB = 'ccc_emergency_map'
--- CCC_app/config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY', 'your_secret_key_here')
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://username:password@localhost/ccc_emergency_map'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
