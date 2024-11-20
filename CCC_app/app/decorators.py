6a19502e6794add33bb4ebdcf871afa904a74dfa
BDMaurer <125302800+BDMaurer@users.noreply.github.com>
2024-11-20 11:15:18 -0500

    Update decorators.py

--- CCC_app/app/decorators.py
+++ CCC_app/app/decorators.py
@@ -1,34 +1,24 @@
 ﻿# decorators.py
-from functools import wraps
-from flask import redirect, url_for, flash
-from flask_login import current_user
+from functools import wraps 
 
-# Decorator to check if the user is an admin
-def admin_required(f):
-    @wraps(f)
-    def decorated_function(*args, **kwargs):
-        if not current_user.is_authenticated or not current_user.is_admin:
-            flash('You do not have the required permissions to access this page.', 'danger')
-            return redirect(url_for('login'))
-        return f(*args, **kwargs)
-    return decorated_function
+from flask import abort 
 
-# Decorator to check if the user is a super admin
-def super_admin_required(f):
-    @wraps(f)
-    def decorated_function(*args, **kwargs):
-        if not current_user.is_authenticated or not current_user.is_super_admin:
-            flash('You do not have the required permissions to access this page.', 'danger')
-            return redirect(url_for('login'))
-        return f(*args, **kwargs)
-    return decorated_function
+from flask_login import current_user 
 
-# Decorator to check if the user is an employee
-def employee_required(f):
-    @wraps(f)
-    def decorated_function(*args, **kwargs):
-        if not current_user.is_authenticated or not current_user.is_employee:
-            flash('You do not have the required permissions to access this page.', 'danger')
-            return redirect(url_for('login'))
-        return f(*args, **kwargs)
-    return decorated_function
+def permission_required(permission): 
+
+    def decorator(f): 
+
+        @wraps(f) 
+
+        def decorated_function(*args, **kwargs): 
+
+            if current_user.PermissionTier != permission: 
+
+                abort(403) 
+
+            return f(*args, **kwargs) 
+
+        return decorated_function 
+
+    return decorator 
